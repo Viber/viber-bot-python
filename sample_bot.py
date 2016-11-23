31c6a2e3bc4532c6cae52da1a4208ced6abbd678
Itamar <ItamarM@users.noreply.github.com>
2016-11-23 15:43:51 +0200

    Add sample bot

---
+++ sample_bot.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, Response
+from viber.api.api import Api
+from viber.api.bot_configuration import BotConfiguration
+from viber.api.messages.text_message import TextMessage
+import time
+import logging
+import sched
+import threading
+
+from viber.api.viber_requests import ViberConversationStartedRequest
+from viber.api.viber_requests import ViberFailedRequest
+from viber.api.viber_requests import ViberMessageRequest
+from viber.api.viber_requests import ViberSubscribedRequest
+from viber.api.viber_requests import ViberUnsubscribedRequest
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+app = Flask(__name__)
+viber = Api(BotConfiguration(
+  name='PythonSampleBot',
+  avatar='http://viber.com/avatar.jpg',
+  auth_token='YOUR_AUTH_TOKEN_HERE'
+))
+
+@app.route('/', methods=['POST'])
+def incoming():
+	logger.debug("received request. post data: {0}".format(request.get_data()))
+
+	viber_request = viber.parse_request(request.get_data())
+
+	if isinstance(viber_request, ViberMessageRequest):
+		message = viber_request.get_message()
+		viber.send_messages(viber_request.get_sender().get_id(), [
+			message
+		])
+	elif isinstance(viber_request, ViberConversationStartedRequest) \
+			or isinstance(viber_request, ViberSubscribedRequest) \
+			or isinstance(viber_request, ViberUnsubscribedRequest):
+		viber.send_messages(viber_request.get_user().get_id(), [
+			TextMessage(None, None, viber_request.get_event_type())
+		])
+	elif isinstance(viber_request, ViberFailedRequest):
+		logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+
+	return Response(status=200)
+
+def set_webhook(viber):
+	viber.set_webhook('https://mybotwebserver.com:8443/')
+
+
+if __name__ == "__main__":
+	scheduler = sched.scheduler(time.time, time.sleep)
+	scheduler.enter(5, 1, set_webhook, (viber,))
+	t = threading.Thread(target=scheduler.run)
+	t.start()
+
+	context = ('server.crt', 'server.key')
+	app.run(host='0.0.0.0', port=8443, debug=True, ssl_context=context)
