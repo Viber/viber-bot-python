41f8bb9ddf5e5702f6e084bdc68c7e6d267bd561
lidora <lidora@viber.com>
2016-11-30 15:37:26 +0200

    changed version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
 	name='viber',
-	version='0.1.3',
+	version='0.1.3.1',
 	packages=['viber', 'viber.api', 'viber.api.viber_requests',
 			  'viber.api.messages', 'viber.api.messages.data_types'],
 	install_requires=['future', 'requests'],
 	url='https://github.com/Viber/viber-bot-python',
-	download_url='https://github.com/Viber/viber-bot-python/tarball/0.1.3'
+	download_url='https://github.com/Viber/viber-bot-python/tarball/0.1.3.1'
 )
