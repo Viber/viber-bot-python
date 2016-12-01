91e0b6c6ba4baa93a96a95ca82a91f6995cf55a4
lidora <lidora@viber.com>
2016-12-01 12:55:19 +0200

    changed module viber to viberbot

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-	name='viber',
+	name='viberbot',
 	version='0.1.3.1',
-	packages=['viber', 'viber.api', 'viber.api.viber_requests',
-			  'viber.api.messages', 'viber.api.messages.data_types'],
+	packages=['viberbot', 'viberbot.api', 'viberbot.api.viber_requests',
+			  'viberbot.api.messages', 'viberbot.api.messages.data_types'],
 	install_requires=['future', 'requests'],
 	url='https://github.com/Viber/viber-bot-python',
 	download_url='https://github.com/Viber/viber-bot-python/tarball/0.1.3.1'
