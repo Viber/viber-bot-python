7922aa5194e673deb69c80822e587af3879fc58d
wowkin2 <djwowkin@gmail.com>
2020-08-21 13:06:54 +0300

    Fixed typo `meesage_token` (`e` -> `s`) in ViberFailedRequest and ViberSeenRequest

--- viberbot/api/viber_requests/viber_seen_request.py
+++ viberbot/api/viber_requests/viber_seen_request.py
@@ -1,3 +1,4 @@
+import warnings
 from future.utils import python_2_unicode_compatible
 from ..event_type import EventType
 from viberbot.api.viber_requests.viber_request import ViberRequest
@@ -17,6 +18,11 @@ class ViberSeenRequest(ViberRequest):
 
 	@property
 	def meesage_token(self):
+		warnings.warn('Property `meesage_token` had typo and now is deprecated, please use `message_token` instead')
+		return self._message_token
+
+	@property
+	def message_token(self):
 		return self._message_token
 
 	@property
