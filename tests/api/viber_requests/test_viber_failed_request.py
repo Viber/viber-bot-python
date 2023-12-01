b5274df08f1a1e7b2aac9404bf4a8ce7e3243613
Justas Kalpokas <justas@datanome.ai>
2023-12-01 16:35:43 +0200

    update tests

--- tests/api/viber_requests/test_viber_failed_request.py
+++ tests/api/viber_requests/test_viber_failed_request.py
@@ -5,20 +5,20 @@ from viberbot.api.viber_requests import ViberFailedRequest
 from viberbot.api.viber_requests import create_request
 
 SAMPLE_REQUEST = dict(
-	event=EventType.FAILED,
-	timestamp=datetime.now(),
-	message_token="912661846655238145",
-	user_id="01234567890A=",
-	desc="failure desc"
+    event=EventType.FAILED,
+    timestamp=datetime.now(),
+    message_token="912661846655238145",
+    user_id="01234567890A=",
+    desc="failure desc",
 )
 
 
 def test_create_request():
-	request = create_request(SAMPLE_REQUEST)
+    request = create_request(SAMPLE_REQUEST)
 
-	assert isinstance(request, ViberFailedRequest)
-	assert request.event_type == SAMPLE_REQUEST['event']
-	assert request.timestamp == SAMPLE_REQUEST['timestamp']
-	assert request.meesage_token == SAMPLE_REQUEST['message_token']
-	assert request.user_id == SAMPLE_REQUEST['user_id']
-	assert request.desc == SAMPLE_REQUEST['desc']
+    assert isinstance(request, ViberFailedRequest)
+    assert request.event_type == SAMPLE_REQUEST["event"]
+    assert request.timestamp == SAMPLE_REQUEST["timestamp"]
+    assert request.message_token == SAMPLE_REQUEST["message_token"]
+    assert request.user_id == SAMPLE_REQUEST["user_id"]
+    assert request.desc == SAMPLE_REQUEST["desc"]
